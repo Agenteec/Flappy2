4fb4e188bc064a593f9ed937321e551d504271f2
Agenteec <g.mens.hl@gmail.com>
2023-06-13 23:28:41 +0300

    Start

---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include <SFML/Graphics.hpp>
+#include "imgui.h"
+#include "imgui-SFML.h"
+int main()
+{
+	sf::RenderWindow w(sf::VideoMode(640,480),L"Работает");
+	system("pause");
+	return 0;
+}
---
+++ Window.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+class Window
+{
+	float WinH;
+	float WinW;
+	sf::RenderWindow window;
+public:
+	Window():WinW(1024.f), WinH(768.f)
+	{
+		
+	}
+	void Render()
+	{
+
+	}
+};
+
